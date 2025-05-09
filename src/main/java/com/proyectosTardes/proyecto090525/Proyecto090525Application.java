3da7887e9a523bfc7bc343676ce38ace44d08ae7
pmorenateide <pmorena@teideformacion.com>
2025-05-09 16:01:09 +0200

    Add files via upload

---
+++ src/main/java/com/proyectosTardes/proyecto090525/Proyecto090525Application.java
@@ -0,0 +1,13 @@
+package com.proyectosTardes.proyecto090525;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto090525Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto090525Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectosTardes/proyecto090525/Proyecto090525ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectosTardes.proyecto090525;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto090525ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
